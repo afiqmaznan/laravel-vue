52e7a91cafb5af6e6b8049fa6205f29cb78a818b
afiqmaznan <46741144+afiqmaznan@users.noreply.github.com>
2021-11-13 03:17:54 +0800

    Update routing from welcome page to places page

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -33,6 +33,13 @@
                     <path d="M204.8 0H256L128 220.8 0 0h97.92L128 51.2 157.44 0h47.36z" fill="#41B883"/><path d="M0 0l128 220.8L256 0h-51.2L128 132.48 50.56 0H0z" fill="#41B883"/><path d="M50.56 0L128 133.12 204.8 0h-47.36L128 51.2 97.92 0H50.56z" fill="#35495E"/>
                 </svg>
             </div>
+            <div class="mx-auto mt-6 sm:items-center sm:justify-between">
+                <div class="box-border md:box-content p-0">
+                    <Link :href="route('places.index')" class="text-xl text-gray-700 text-center underline">
+                        <p>Click here to continue</p>
+                    </Link>
+                </div>
+            </div>
 
             <!-- <div class="mt-8 bg-white dark:bg-gray-800 overflow-hidden shadow sm:rounded-lg">
                 <div class="grid grid-cols-1 md:grid-cols-2">
